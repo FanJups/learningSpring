75a7a8429eca2526231f914cd5a79c815eb42827
FanJups <jupsfan@gmail.com>
2018-12-28 18:09:36 +0100

    Uploading on GITHUB

---
+++ SpringExampleLog4J/src/com/tutorialspoint/MainApp.java
@@ -0,0 +1,24 @@
+package com.tutorialspoint;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class MainApp {
+
+	static Log log = LogFactory.getLog(MainApp.class.getName());
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		ApplicationContext context = new ClassPathXmlApplicationContext("Beans.xml");
+		log.info("Going to create HelloWord Obj");
+		HelloWorld obj = (HelloWorld) context.getBean("helloWorld");
+		obj.getMessage();
+
+		log.info("Exiting the program");
+
+	}
+
+}
